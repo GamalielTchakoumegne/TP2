f984ec0d2b4ca78cb551c153e27e4814cf2791ef
GamalielTchakoumegne <tchakoumegnegamaliel@gmail.com>
2023-09-21 10:18:07 -0400

    Apres NuGets

---
+++ Application/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2.Donnee;
+
+namespace TP2.Application
+{
+    public class Application
+    {
+    }
+}
---
+++ Presentation/Presentation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2.Application;
+
+namespace TP2.Presentation
+{
+    public class Presentation
+    {
+        Console.WriteLine("Quel est le nom ?");
+    }
+}
